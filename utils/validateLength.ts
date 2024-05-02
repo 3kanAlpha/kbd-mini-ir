6e95a93068418c7445ddd558b44e8d9e906abee3
3kanAlpha <39447635+3kanAlpha@users.noreply.github.com>
2024-05-03 03:04:44 +0900

    update: プレイヤー名の文字数最大値の変更

--- utils/validateLength.ts
+++ utils/validateLength.ts
@@ -1,6 +1,6 @@
 /** 与えられた文字列sの長さがlengthLimit以下かどうか検証する */
 export default function (s: string, lengthLimit: number): boolean | string {
-  if (s.trim().length > lengthLimit) {
+  if (s.length > lengthLimit) {
     return `この項目は${lengthLimit}文字以下で入力してください。`
   }
 
